7e4432504a5a3893fab047cdb396b63220eedc84
Aoura10 <manoj885chaudhary@gmail.com>
2026-09-04 15:35:45 +0530

    Initial commit

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -14,16 +14,22 @@ public class Lander : MonoBehaviour
     private void FixedUpdate()
     {
         // for input system package
-        if(Keyboard.current.upArrowKey.isPressed){
-            landerRigidbody2D.AddForce(transform.up * Time.deltatime);
+        if(Keyboard.current.wKey.isPressed)
+        {
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.fixedDeltaTime);
         }
 
-        if(Keyboard.current.leftArrowKey.isPressed){
-            Debug.Log("Left");
+        if(Keyboard.current.aKey.isPressed)
+        {
+            float turnSpeed = +100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.fixedDeltaTime);
         }
 
-        if(Keyboard.current.rightArrowKey.isPressed){
-            Debug.Log("Right");
+        if(Keyboard.current.dKey.isPressed)
+        {
+            float turnSpeed = -100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.fixedDeltaTime);
         }
 
         // for input manager
