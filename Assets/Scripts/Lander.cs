ae6599508efe7ba90049c55c8a92fcff9799659f
Aoura10 <manoj885chaudhary@gmail.com>
2026-09-05 15:11:31 +0530

    Initial commit

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -22,26 +22,33 @@ public class Lander : MonoBehaviour
         // for input system package
         if(Keyboard.current.wKey.isPressed)
         {
-            float force = 700f;
+            float force = 500f;
             landerRigidbody2D.AddForce(force * transform.up * Time.fixedDeltaTime);
         }
 
         if(Keyboard.current.aKey.isPressed)
         {
-            float turnSpeed = +100f;
+            float turnSpeed = +80f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.fixedDeltaTime);
         }
 
         if(Keyboard.current.dKey.isPressed)
         {
-            float turnSpeed = -100f;
+            float turnSpeed = -80f;
             landerRigidbody2D.AddTorque(turnSpeed * Time.fixedDeltaTime);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D Collision2D)
     {
+        if (!Collision2D.gameObject.TryGetComponent(out LandingPad langingPad))
+        {
+            Debug.Log("Crashed on the Terrain!");
+            return;
+        }
+
         float softLandingVelocityMagnitude = 4f;
+        float relativeVelocityMagnitude = Collision2D.relativeVelocity.magnitude;
         if (Collision2D.relativeVelocity.magnitude > softLandingVelocityMagnitude)
         {
             // Landed too hard!
@@ -58,6 +65,16 @@ public class Lander : MonoBehaviour
             return;
         }
             
-        Debug.Log("Successful Landing!");   
+        Debug.Log("Successful Landing!");
+
+        float maxScoreAmountLandingAngle = 100;
+        float scoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier * maxScoreAmountLandingAngle;
+        
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
+
+        Debug.Log("landingAngleScore: " + landingAngleScore);
+        Debug.Log("landingSpeedScore: " + landingSpeedScore);
     }
 }
