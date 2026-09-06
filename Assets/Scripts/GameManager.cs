5245d413a9aa2468dc1d346d56392949df21b553
Aoura10 <manoj885chaudhary@gmail.com>
2026-09-06 14:17:00 +0530

    Initial commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 
-public class GeneralGameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance {get; private set;}
+
     private int score;
+    private float time;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         Lander.Instance.OnCoinPickup += Lander_OnCoinPickup;
         Lander.Instance.OnLanded += Lander_OnLanded;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
     }
 
     private void Lander_OnLanded(object sender, Lander.OnLandedEventArgs e)
@@ -27,4 +39,13 @@ public class GeneralGameManager : MonoBehaviour
         Debug.Log(score);
     }
 
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
 }
