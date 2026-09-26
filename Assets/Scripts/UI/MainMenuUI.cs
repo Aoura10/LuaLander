abcdf20630311cf3b5b76e7cc7c0fb0c2bb1369e
Aoura10 <manoj885chaudhary@gmail.com>
2026-09-26 10:11:22 +0530

    Initial commit

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+}
